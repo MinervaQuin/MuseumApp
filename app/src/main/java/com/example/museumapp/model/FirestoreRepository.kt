56a01a7c2c9164a76c2c8fe982192cf62c5fba09
Andrew-022 <145354381+Andrew-022@users.noreply.github.com>
2023-12-10 23:19:18 +0000

    convert localdate

--- app/src/main/java/com/example/museumapp/model/FirestoreRepository.kt
+++ app/src/main/java/com/example/museumapp/model/FirestoreRepository.kt
@@ -15,4 +15,7 @@ interface FirestoreRepository {
 
     suspend fun uploadWorktoFirestore()
 
+    suspend fun timestampToLocalDate(timestamp: Timestamp): LocalDate
+    suspend fun convertLocalDateToTimestamp(localDate: LocalDate): Timestamp
+
 }
--- app/src/main/java/com/example/museumapp/model/firebaseAuth/FirestoreRepositoryImpl.kt
+++ app/src/main/java/com/example/museumapp/model/firebaseAuth/FirestoreRepositoryImpl.kt
@@ -87,12 +87,19 @@ class FirestoreRepositoryImpl @Inject constructor(private val firebaseFirestore:
     }
 
 
-    fun convertLocalDateToTimestamp(localDate: LocalDate): Timestamp {
+    override suspend fun convertLocalDateToTimestamp(localDate: LocalDate): Timestamp {
         val zoneId = ZoneId.systemDefault()
         val epochMillis = localDate.atStartOfDay(zoneId).toInstant().toEpochMilli()
         return Timestamp(epochMillis / 1000, ((epochMillis % 1000) * 1_000_000).toInt())
     }
+    override suspend fun timestampToLocalDate(timestamp: Timestamp): LocalDate {
 
+        val date = Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000)
+
+        return date.toInstant()
+            .atZone(ZoneId.systemDefault())
+            .toLocalDate()
+    }
     override suspend fun uploadWorktoFirestore() {
         val newWork = WorkFb("Saturno Devorando a su hijo","Francisco de Goya","", convertLocalDateToTimestamp(LocalDate.now()), "https://s1.ppllstatics.com/laverdad/www/multimedia/202211/20/media/cortadas/174982724--1248x1888.jpg","Es una de las obras de mayor tamaño de Velázquez y en la que puso un mayor empeño para crear una composición a la vez compleja y creíble, que transmitiera la sensación de vida y realidad, y al mismo tiempo encerrara una densa red de significados. El pintor alcanzó su objetivo y el cuadro se convirtió en la única pintura a la que el tratadista Antonio Palomino dedicó un epígrafe en su historia de los pintores españoles (1724). Lo tituló En que se describe la más ilustre obra de don Diego Velázquez, y desde entonces no ha perdido su estatus de obra maestra. Gracias a Palomino sabemos que se pintó en 1656 en el Cuarto del Príncipe del Alcázar de Madrid, que es el escenario de la acción. El tratadista cordobés también identificó a la mayor parte de los personajes: son servidores palaciegos, que se disponen alrededor de la infanta Margarita, a la que atienden doña María Agustina Sarmiento y doña Isabel de Velasco, meninas de la reina. Además de ese grupo, vemos a Velázquez trabajar ante un gran lienzo, a los enanos Mari Bárbola y Nicolasito Pertusato, que azuza a un mastín, a la dama de honor doña Marcela de Ulloa, junto a un guardadamas, y, al fondo, tras la puerta, asoma José Nieto, aposentador. En el espejo se ven reflejados los rostros de Felipe IV y Mariana de Austria, padres de la infanta y testigos de la escena. Los personajes habitan un espacio modelado no sólo mediante las leyes de la perspectiva científica sino también de la perspectiva aérea, en cuya definición representa un papel importante la multiplicación de las fuentes de luz.")
 
